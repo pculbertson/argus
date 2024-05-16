d07243d22cf099ee37128f3e3cf20c32bab6b7c9
alberthli <alberthli@caltech.edu>
2024-05-16 16:23:29 -0700

    added some utils for quat convention conversions

--- argus/utils.py
+++ argus/utils.py
@@ -2,6 +2,53 @@ from typing import Callable
 
 import torch
 
+# ###################### #
+# CONVENTION CONVERSIONS #
+# ###################### #
+
+
+def xyzwxyz_to_xyzxyzw_SE3(xyzwxyz: torch.Tensor) -> torch.Tensor:
+    """Converts a tensor of 7d poses with quats from (w, x, y, z) to (x, y, z, w) order.
+
+    Args:
+        xyzwxyz: The tensor of poses of shape (..., 7) whose quats are in (w, x, y, z) order.
+
+    Returns:
+        xyzxyzw: The tensor now with (x, y, z, w) order.
+    """
+    return torch.cat(
+        (
+            xyzwxyz[..., :3],  # translations
+            xyzwxyz[..., -3:],  # the (qx, qy, qz) components
+            xyzwxyz[..., -4],  # the qw component
+        ),
+        dim=-1,
+    )
+
+
+def xyzxyzw_to_xyzwxyz_SE3(xyzxyzw: torch.Tensor) -> torch.Tensor:
+    """Converts a tensor of 7d poses with quats from (x, y, z, w) to (w, x, y, z) order.
+
+    Args:
+        xyzxyzw: The tensor of poses of shape (..., 7) whose quats are in (x, y, z, w) order.
+
+    Returns:
+        xyzwxyz: The tensor now with (w, x, y, z) order.
+    """
+    return torch.cat(
+        (
+            xyzxyzw[..., :3],  # translations
+            xyzxyzw[..., -1:],  # the qw component
+            xyzxyzw[..., -4:-1],  # the (qx, qy, qz) components
+        ),
+        dim=-1,
+    )
+
+
+# ########## #
+# EVALUATION #
+# ########## #
+
 
 def time_torch_fn(fn: Callable[[], torch.Tensor]) -> tuple[torch.Tensor, float]:
     """Time a torch function.
