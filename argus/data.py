8c93218ad3f9ae03a8f3d94211a7c67ddd1e3cbe
alberthli <alberthli@caltech.edu>
2024-05-24 17:36:26 -0700

    minor

--- argus/data.py
+++ argus/data.py
@@ -211,7 +211,7 @@ if __name__ == "__main__":
     import cv2
     import tyro
 
-    dataset_cfg = CameraCubePoseDatasetConfig(dataset_path=ROOT + "/outputs/data/cube_unity_data_small.hdf5")
+    dataset_cfg = CameraCubePoseDatasetConfig(dataset_path=ROOT + "/outputs/data/cube_unity_data_small")
     augmentation_cfg = tyro.cli(AugmentationConfig)
     train_dataset = CameraCubePoseDataset(dataset_cfg, train=True)
 
