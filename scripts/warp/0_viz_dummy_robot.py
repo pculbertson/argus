2da414c51716739173f80059a8434fe03a9ffa2b
alberthli <alberthli@caltech.edu>
2024-07-01 15:50:50 -0700

    sanity check integrator example

--- scripts/warp/0_viz_dummy_robot.py
+++ scripts/warp/0_viz_dummy_robot.py
@@ -6,6 +6,7 @@ import warp.sim.render
 
 from argus import ROOT
 
+# building the model
 builder = wp.sim.ModelBuilder()
 path = f"{ROOT}/scripts/warp/dummy.urdf"
 wp.sim.parse_urdf(
@@ -14,12 +15,29 @@ wp.sim.parse_urdf(
     floating=False,
     enable_self_collisions=False,
     collapse_fixed_joints=True,
+    density=1000,  # these settings taken from the quadruped example - critical!
+    armature=0.01,
+    stiffness=200,
+    damping=1,
+    contact_ke=1.0e4,
+    contact_kd=1.0e2,
+    contact_kf=1.0e2,
+    contact_mu=1.0,
+    limit_ke=1.0e4,
+    limit_kd=1.0e1,
 )
 model = builder.finalize(requires_grad=True)
 model.ground = False
+state = model.state()
 
 # renderer
-renderer = wp.sim.render.SimRendererOpenGL(path=path, model=model, scaling=15.0)
+renderer = wp.sim.render.SimRendererOpenGL(path=path, model=model, scaling=15.0, up_axis="Z")
+
+# setting up the integrator and controller
+# the SemiImplicit integrator only integrates body coordinates - need to call IK on the
+# state after to retrieve joint coordinates. The Featherstone integrator takes care of it.
+integrator = wp.sim.FeatherstoneIntegrator(model)
+control = model.control()
 
 
 def render(time, state):
@@ -34,86 +52,13 @@ def render(time, state):
 with wp.ScopedDevice("cuda"):
     t = 0
     while True:
-        render(t, model.state())
+        # update the model state to be moving sinusoidally
+        state.clear_forces()
+        control.joint_act = wp.array([np.sin(t / 1000)], dtype=wp.float32)
+        integrator.simulate(model, state, state, 0.001, control=control)
+        if np.isnan(state.joint_q.numpy()).any():
+            breakpoint()  # this block checks for integrator instability
+        model.joint_q = state.joint_q
+        model.joint_qd = state.joint_qd
+        render(t, state)
         t += 1
-
-breakpoint()
-
-
-# class Example:
-#     def __init__(self):
-#         # renderer
-#         self.renderer = wp.render.OpenGLRenderer(vsync=False)
-#         self.renderer.render_ground()
-
-#         # model
-#         builder = wp.sim.ModelBuilder()
-#         wp.sim.parse_urdf(
-#             f"{ROOT}/scripts/warp/dummy.urdf",
-#             builder,
-#             enable_self_collisions=False,
-#             collapse_fixed_joints=True,
-#         )
-#         self.model = builder.finalize(requires_grad=True)
-#         self.model.ground = False
-
-#     def render(self):
-#         time = self.renderer.clock_time
-#         self.renderer.begin_frame(time)
-
-#         self.renderer.end_frame()
-
-
-# if __name__ == "__main__":
-#     import argparse
-#     import distutils.util
-
-#     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-#     parser.add_argument("--device", type=str, default=None, help="Override the default Warp device.")
-#     parser.add_argument("--num_tiles", type=int, default=4, help="Number of viewports to render in a single frame.")
-#     parser.add_argument(
-#         "--show_plot",
-#         type=lambda x: bool(distutils.util.strtobool(x.strip())),
-#         default=True,
-#         help="Display the pixels in an additional matplotlib figure.",
-#     )
-#     parser.add_argument("--render_mode", type=str, choices=("depth", "rgb"), default="depth", help="")
-#     parser.add_argument(
-#         "--split_up_tiles",
-#         type=lambda x: bool(distutils.util.strtobool(x.strip())),
-#         default=True,
-#         help="Whether to split tiles into subplots when --show_plot is True.",
-#     )
-#     parser.add_argument("--custom_tile_arrangement", action="store_true", help="Apply custom tile arrangement.")
-
-#     args = parser.parse_known_args()[0]
-
-#     with wp.ScopedDevice(args.device):
-#         example = Example(num_tiles=args.num_tiles, custom_tile_arrangement=args.custom_tile_arrangement)
-
-#         channels = 1 if args.render_mode == "depth" else 3
-#         while example.renderer.is_running():
-#             example.render()
-
-#             if args.show_plot and plt.fignum_exists(1):
-#                 if args.split_up_tiles:
-#                     pixel_shape = (args.num_tiles, example.renderer.tile_height, example.renderer.tile_width, channels)
-#                 else:
-#                     pixel_shape = (example.renderer.screen_height, example.renderer.screen_width, channels)
-
-#                 if pixel_shape != pixels.shape:
-#                     # make sure we resize the pixels array to the right dimensions if the user resizes the window
-#                     pixels = wp.zeros(pixel_shape, dtype=wp.float32)
-
-#                 example.renderer.get_pixels(pixels, split_up_tiles=args.split_up_tiles, mode=args.render_mode)
-
-#                 if args.split_up_tiles:
-#                     pixels_np = pixels.numpy()
-#                     for i, img_plot in enumerate(img_plots):
-#                         img_plot.set_data(pixels_np[i])
-#                 else:
-#                     img_plot.set_data(pixels.numpy())
-#                 fig.canvas.draw()
-#                 fig.canvas.flush_events()
-
-#         example.renderer.clear()
