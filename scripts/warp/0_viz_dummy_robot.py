13ea5e7d3624bb695cdfa83b5c1e8e9e22878639
alberthli <alberthli@caltech.edu>
2024-07-01 17:06:36 -0700

    upgrade dummy example with a toy cube

--- scripts/warp/0_viz_dummy_robot.py
+++ scripts/warp/0_viz_dummy_robot.py
@@ -26,12 +26,38 @@ wp.sim.parse_urdf(
     limit_ke=1.0e4,
     limit_kd=1.0e1,
 )
+cube_size = 0.035
+cube = builder.add_body()
+builder.add_joint_free(cube)
+builder.add_shape_box(
+    cube,
+    hx=cube_size,
+    hy=cube_size,
+    hz=cube_size,
+    ke=1e5,
+    kd=250.0,
+    kf=500.0,
+)
 model = builder.finalize(requires_grad=True)
 model.ground = False
+model.gravity[:] = 0.0  # no gravity
+model.joint_attach_ke = 16000.0
+model.joint_attach_kd = 200.0
 state = model.state()
 
+# update state
+q0 = np.zeros(8)  # creating state for a single robot
+q0[0] = 0.5  # robot arm rotated 0.5 radians
+q0[1] = 0.5  # x position of the cube is 0.5 meters out
+q0[2] = -0.12  # y pos off center
+q0[7] = 1.0  # w coordinate of the quaternion
+q0 = wp.from_numpy(q0, device="cuda", dtype=wp.float32)  # (8,)
+qd0 = wp.zeros(model.joint_dof_count, device="cuda")  # (14,)
+wp.sim.eval_fk(model, q0, qd0, None, state)  # updates body states given initial joint states
+wp.sim.eval_ik(model, state, state.joint_q, state.joint_qd)  # updates joint states w/body states
+
 # renderer
-renderer = wp.sim.render.SimRendererOpenGL(path=path, model=model, scaling=15.0, up_axis="Z")
+renderer = wp.sim.render.SimRendererOpenGL(path=path, model=model, scaling=1.0, up_axis="Z")
 
 # setting up the integrator and controller
 # the SemiImplicit integrator only integrates body coordinates - need to call IK on the
@@ -53,12 +79,11 @@ with wp.ScopedDevice("cuda"):
     t = 0
     while True:
         # update the model state to be moving sinusoidally
-        state.clear_forces()
         control.joint_act = wp.array([np.sin(t / 1000)], dtype=wp.float32)
+        state.clear_forces()
+        wp.sim.collide(model, state)
         integrator.simulate(model, state, state, 0.001, control=control)
         if np.isnan(state.joint_q.numpy()).any():
             breakpoint()  # this block checks for integrator instability
-        model.joint_q = state.joint_q
-        model.joint_qd = state.joint_qd
         render(t, state)
         t += 1
